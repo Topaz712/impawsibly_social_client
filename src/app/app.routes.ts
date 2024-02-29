f9675300bbb0111cb638aa9eb7f790c76cfaea3c
Topaz712 <tzam712@gmail.com>
2024-02-29 13:16:46 -0600

    CREATED: Auth Guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,13 +3,17 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
+    redirectTo: 'login',
     pathMatch: 'full',
-    loadComponent: () =>
-      import('./features/home/home.component').then((c) => c.HomeComponent),
   },
   {
     path: 'login',
     loadComponent: () =>
       import('./features/login/login.component').then((c) => c.LoginComponent),
+  },
+  {
+    path: 'home',
+    loadComponent: () =>
+      import('./features/home/home.component').then((c) => c.HomeComponent),
   },
 ];
