f5f2a1f47341ca2e8d3fef9be7f5e6f22708da7e
Topaz712 <tzam712@gmail.com>
2024-03-07 05:13:46 -0600

    ADD: Navbar/logout function

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -11,7 +11,9 @@ export const routes: Routes = [
   {
     path: 'login',
     loadComponent: () =>
-      import('./features/login/login.component').then((c) => c.LoginComponent),
+      import('./features/auth/login/login.component').then(
+        (c) => c.LoginComponent
+      ),
     canActivate: [noAuthGuard],
   },
   {
