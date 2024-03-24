32900436e3de0f9e2a14723945d7a61d3137eb29
Topaz712 <tzam712@gmail.com>
2024-03-23 23:25:55 -0500

    ADD: pet model

---
+++ src/app/shared/models/pet.ts
@@ -0,0 +1,21 @@
+export class Pet {
+  id: number;
+  name: string;
+  species: string;
+  breed: string;
+  sex: string;
+  birthday: Date;
+  isVaccinated: boolean;
+  isFixed: boolean;
+
+  constructor(pet: any) {
+    this.id = pet.id;
+    this.name = pet.name;
+    this.species = pet.species;
+    this.breed = pet.breed;
+    this.sex = pet.sex;
+    this.birthday = pet.birthday;
+    this.isVaccinated = pet.isVaccinated;
+    this.isFixed = pet.isFixed;
+  }
+}
