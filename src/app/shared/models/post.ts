0ff6321dd4cf32c73208fd24d9537943ade16eaa
Topaz712 <tzam712@gmail.com>
2024-03-26 00:02:31 -0500

    ADD: httpclient to send requests

--- src/app/shared/models/post.ts
+++ src/app/shared/models/post.ts
@@ -2,14 +2,12 @@ import { Pet } from './pet';
 
 export class Post {
   id: number;
-  title: string;
   content: string;
   created_at: string;
   pet?: Pet;
 
   constructor(post: any) {
     this.id = post.id || 0;
-    this.title = post.title || '';
     this.content = post.content || '';
     this.created_at = post.created_at || '';
     this.pet = post.pet || null;
