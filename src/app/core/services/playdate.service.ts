10881ceebaa8fa76101e6cd1fc846513757e4c0e
Topaz712 <tzam712@gmail.com>
2024-04-01 23:59:55 -0500

    ADD: api endpoint to create playdates

--- src/app/core/services/playdate.service.ts
+++ src/app/core/services/playdate.service.ts
@@ -1,9 +1,25 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from '../../../environments/environment';
+import { Playdate } from '../../shared/models/playdate';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PlaydateService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  getPlaydates(page: number) {
+    return this.http.get<Playdate[]>(
+      `${environment.apiUrl}/playdates?page=${page}`
+    );
+  }
+
+  getPlaydate(id: string | number) {
+    return this.http.get<Playdate>(`${environment.apiUrl}/playdates/${id}`);
+  }
+
+  createPlaydate(playdate: Playdate) {
+    return this.http.post(`${environment.apiUrl}/playdates`, playdate);
+  }
 }
