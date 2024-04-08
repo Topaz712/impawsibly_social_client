b9f5f110d8de073dbe8a39f253806a1904409347
Topaz712 <tzam712@gmail.com>
2024-04-07 23:50:51 -0500

    FIXED: front parameter names to match params backend/signup works again

--- src/app/features/auth/signup/signup.component.ts
+++ src/app/features/auth/signup/signup.component.ts
@@ -13,8 +13,8 @@ import { Router } from '@angular/router';
 export class SignupComponent {
   signupForm: FormGroup = new FormGroup({
     username: new FormControl(''),
-    firstName: new FormControl(''),
-    lastName: new FormControl(''),
+    first_name: new FormControl(''),
+    last_name: new FormControl(''),
     email: new FormControl(''),
     password: new FormControl(''),
     password_confirmation: new FormControl(''),
