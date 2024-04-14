0916a8c37e533c676f9baea0f210c6a21e27235b
Topaz712 <tzam712@gmail.com>
2024-04-13 23:57:39 -0500

    ADD: pagination for pet profiles

--- src/app/features/pets/pets.component.ts
+++ src/app/features/pets/pets.component.ts
@@ -12,6 +12,8 @@ import { Router, ActivatedRoute } from '@angular/router';
   styleUrl: './pets.component.scss',
 })
 export class PetsComponent implements OnInit {
+  currentPage: number = 1;
+  totalPages: number = 0;
   pets: Pet[] = [];
 
   constructor(
@@ -21,12 +23,43 @@ export class PetsComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    this.route.queryParams.subscribe(() => {
-      this.loadPets();
+    this.route.queryParams.subscribe((params) => {
+      const page = params['page'] ? Number(params['page']) : 1;
+      this.loadPets(page);
     });
   }
 
-  loadPets() {
-    this.petService.getPets();
+  loadPets(page: number) {
+    this.petService.getPets(page).subscribe({
+      next: (response: any) => {
+        this.pets = response.pets;
+        this.currentPage = response.current_page;
+        this.totalPages = response.total_pages;
+        console.log(this.pets, this.currentPage, this.totalPages);
+      },
+      error: (error: any) => {
+        console.log('error when fetching pets', error);
+      },
+    });
+  }
+
+  nextPage() {
+    if (this.currentPage < this.totalPages) {
+      this.router.navigate([], {
+        relativeTo: this.route,
+        queryParams: { page: this.currentPage + 1 },
+        queryParamsHandling: 'merge',
+      });
+    }
+  }
+
+  previousPage() {
+    if (this.currentPage > 1) {
+      this.router.navigate([], {
+        relativeTo: this.route,
+        queryParams: { page: this.currentPage - 1 },
+        queryParamsHandling: 'merge',
+      });
+    }
   }
 }
--- src/app/shared/components/posts/post/post.component.ts
+++ src/app/shared/components/posts/post/post.component.ts
@@ -2,6 +2,7 @@ import { Component, Input } from '@angular/core';
 import { Post } from '../../../models/post';
 import { DatePipe } from '@angular/common';
 
+
 @Component({
   selector: 'app-post',
   standalone: true,
@@ -11,4 +12,5 @@ import { DatePipe } from '@angular/common';
 })
 export class PostComponent {
   @Input({ required: true }) post: Post = new Post({});
+
 }
