c6352953b5b8c7ff70a5107eb5f781cb69540143
Topaz712 <tzam712@gmail.com>
2024-04-15 15:51:21 -0500

    EDIT: small additions/removal of code snippets

--- src/app/features/pet-posts/pet-posts.component.ts
+++ src/app/features/pet-posts/pet-posts.component.ts
@@ -1,24 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Pet } from '../../shared/models/pet';
 import { User } from '../../shared/models/user';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { PetService } from '../../core/services/pet.service';
 import { UserService } from '../../core/services/user.service';
 import { DatePipe } from '@angular/common';
-
 @Component({
   selector: 'app-pet-posts',
   standalone: true,
-  imports: [DatePipe],
+  imports: [DatePipe, RouterLink],
   templateUrl: './pet-posts.component.html',
   styleUrl: './pet-posts.component.scss',
 })
 export class PetPostsComponent implements OnInit {
-  pet: Pet = new Pet({});
+  @Input() pet: Pet = new Pet({});
   currentUser: User | null = new User({});
 
   constructor(
     private route: ActivatedRoute,
+    private router: Router,
     private petService: PetService,
     private userService: UserService
   ) {}
@@ -40,4 +40,10 @@ export class PetPostsComponent implements OnInit {
       this.currentUser = this.userService.currentUserBehaviorSubject.value;
     });
   }
+
+  onUpdatePet(petId: number) {
+    this.router.navigate(['/update-pet', petId]);
+  }
+
+  onDeletePet() {}
 }
--- src/app/features/pets/pets.component.ts
+++ src/app/features/pets/pets.component.ts
@@ -2,12 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { PetComponent } from '../../shared/components/pets/pet/pet.component';
 import { Pet } from '../../shared/models/pet';
 import { PetService } from '../../core/services/pet.service';
-import { Router, ActivatedRoute } from '@angular/router';
+import { Router, ActivatedRoute, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-pets',
   standalone: true,
-  imports: [PetComponent],
+  imports: [PetComponent, RouterLink],
   templateUrl: './pets.component.html',
   styleUrl: './pets.component.scss',
 })
