347c0bf68bd4225ea9c24935a8a398aeaf634d8d
Topaz712 <tzam712@gmail.com>
2024-04-22 23:29:30 -0500

    ADD: properties to match post api model

--- src/app/shared/models/post.ts
+++ src/app/shared/models/post.ts
@@ -1,10 +1,13 @@
 import { Pet } from './pet';
+import { User } from './user';
 
 export class Post {
   id: number;
   content: string;
   created_at: string;
   pet?: Pet;
+  user?: User;
+  post_image_url: string;
   liked: boolean;
 
   constructor(post: any) {
@@ -12,6 +15,8 @@ export class Post {
     this.content = post.content || '';
     this.created_at = post.created_at || '';
     this.pet = post.pet || null;
+    this.user = post.user || null;
+    this.post_image_url = post.post_image_url;
     this.liked = post.liked || false;
   }
 }
