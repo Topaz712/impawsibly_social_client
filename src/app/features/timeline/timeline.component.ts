ab4f255ec2ea8889174a83ec128daeb5791e3ad3
Topaz712 <tzam712@gmail.com>
2024-04-23 18:53:42 -0500

    ADD: pet object to timeline posts

--- src/app/features/timeline/timeline.component.ts
+++ src/app/features/timeline/timeline.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, input } from '@angular/core';
 import { Post } from '../../shared/models/post';
 import { PostComponent } from '../../shared/components/posts/post/post.component';
 import { PostService } from '../../core/services/post.service';
+import { Pet } from '../../shared/models/pet';
+import { PetService } from '../../core/services/pet.service';
 
 @Component({
   selector: 'app-timeline',
@@ -11,13 +13,18 @@ import { PostService } from '../../core/services/post.service';
   styleUrl: './timeline.component.scss',
 })
 export class TimelineComponent implements OnInit {
+  @Input({ required: true }) pet: Pet = new Pet({});
   posts: Post[] = [];
 
-  constructor(private postService: PostService) {}
+  constructor(
+    private postService: PostService,
+    private petService: PetService
+  ) {}
 
   ngOnInit(): void {
     this.postService.getTimelinePetPosts().subscribe({
       next: (posts: Post[]) => {
+        console.log('this is post:', posts);
         this.posts = posts;
       },
       error: (error: any) => {
