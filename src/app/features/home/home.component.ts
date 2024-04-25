0858c4bf2c29d037222a0769709458540c6affdd
Topaz712 <tzam712@gmail.com>
2024-04-24 22:26:00 -0500

    ADD: pet object to home comp.

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,6 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { LoginComponent } from '../auth/login/login.component';
 import { TimelineComponent } from '../timeline/timeline.component';
+import { User } from '../../shared/models/user';
+import { UserService } from '../../core/services/user.service';
+import { Pet } from '../../shared/models/pet';
 
 @Component({
   selector: 'app-home',
@@ -9,4 +12,8 @@ import { TimelineComponent } from '../timeline/timeline.component';
   styleUrl: './home.component.scss',
   imports: [LoginComponent, TimelineComponent],
 })
-export class HomeComponent {}
+export class HomeComponent {
+  @Input({ required: true }) pet: Pet = new Pet({});
+
+  constructor(userService: UserService) {}
+}
